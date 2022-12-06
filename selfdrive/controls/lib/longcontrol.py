5992a4ef14ba0c4e90dcff84f5d8b834e3ef7b81
Jason Young <jyoung8607@gmail.com>
2022-12-06 18:59:34 -0500

    revert that

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -109,7 +109,11 @@ class LongControl:
         output_accel -= self.CP.stoppingDecelRate * DT_CTRL
       self.reset(CS.vEgo)
 
-    else:
+    elif self.long_control_state == LongCtrlState.starting:
+      output_accel = self.CP.startAccel
+      self.reset(CS.vEgo)
+
+    elif self.long_control_state == LongCtrlState.pid:
       self.v_pid = v_target_now
 
       # Toyota starts braking more when it thinks you want to stop
@@ -117,17 +121,13 @@ class LongControl:
       # TODO too complex, needs to be simplified and tested on toyotas
       prevent_overshoot = not self.CP.stoppingControl and CS.vEgo < 1.5 and v_target_1sec < 0.7 and v_target_1sec < self.v_pid
       deadzone = interp(CS.vEgo, self.CP.longitudinalTuning.deadzoneBP, self.CP.longitudinalTuning.deadzoneV)
-      freeze_integrator = self.long_control_state == LongCtrlState.starting or prevent_overshoot
+      freeze_integrator = prevent_overshoot
 
       error = self.v_pid - CS.vEgo
       error_deadzone = apply_deadzone(error, deadzone)
       output_accel = self.pid.update(error_deadzone, speed=CS.vEgo,
                                      feedforward=a_target,
                                      freeze_integrator=freeze_integrator)
-
-      if self.long_control_state == LongCtrlState.starting:
-        output_accel = max(output_accel, self.CP.startAccel)
-        self.reset(CS.vEgo)
 
     self.last_output_accel = clip(output_accel, accel_limits[0], accel_limits[1])
 
